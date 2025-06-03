486f5a7f9e5fea183de915bfc3a54df0cbe48cfd
SmellyFishPaste <shadowhawk851@gmail.com>
2025-06-03 11:00:39 -0400

    updated the link for the live github page and aded some more per info

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,9 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Nathanael`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>Hello my name is Nathanael I'm looking to secure a challenging position as a Computer Scientist utilizing my expertise in software development,
+                      data analysis, and problem-solving. Looking for a role where I can learn new skills by working with
+                      others. </p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
